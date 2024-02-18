8c83baa462375c4eb7927b769a50c69c7b790098
Kaamya Shinde <kamyaas@stud.ntnu.no>
2024-02-18 17:57:32 +0100

    tests for Complex class

---
+++ test/java/basicLinalg/ComplexTest.java
@@ -0,0 +1,31 @@
+package basicLinalg;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ComplexTest {
+
+    /**
+     * Testing the square root method in the Complex class.
+     */
+    @Test
+    @DisplayName("Square root of a complex number")
+    void sqrt(){
+        Complex c1 = new Complex(4, 4);
+        Complex c2 = c1.sqrt();
+
+        double expectedR = Math.sqrt(32);
+
+        double expectedX0 = (Math.sqrt(expectedR) ) * Math.cos(Math.PI / 8);
+        double expectedX1 = (Math.sqrt(expectedR) ) * Math.sin(Math.PI / 8);
+
+        assertEquals(expectedX0, c2.getX0());
+        assertEquals(expectedX1, c2.getX1());
+
+    }
+
+
+}
