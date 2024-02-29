79330f73061c8ef61c3be457145b1ff1ffb71713
Kevin Dennis Mazali <kevindm@stud.ntnu.no>
2024-02-29 12:43:20 +0100

    javadoc julia adjecent classes

--- src/main/java/edu/ntnu/idatt2003/model/basicLinalg/Matrix2x2.java
+++ src/main/java/edu/ntnu/idatt2003/model/basicLinalg/Matrix2x2.java
@@ -1,5 +1,12 @@
 package edu.ntnu.idatt2003.model.basicLinalg;
 
+/**
+ * Class representing a 2x2 matrix.
+ *
+ * <p> contains a constructor to create a 2x2 matrix object with associated attributes.
+ * The class also includes a method to multiply the matrix with a vector.
+ *
+ */
 public class Matrix2x2 {
 
     private double a00;
@@ -10,6 +17,9 @@ public class Matrix2x2 {
 
     private double a11;
 
+    /**
+     * Constructor for the Matrix2x2 class
+     */
     public Matrix2x2(double inputA00, double inputA01, double inputA10, double inputA11) {
         this.a00 = inputA00;
         this.a01 = inputA01;
@@ -19,6 +29,12 @@ public class Matrix2x2 {
 
     }
 
+    /**
+     *Multiplies a matrix with a vector
+     *
+     * @param vector the vector to multiply with the matrix
+     * @return Vector2D
+     */
     public Vector2D multiply(Vector2D vector) {
         double x = a00 * vector.getX0()+ a01 * vector.getX1();
         double y = a10 * vector.getX0() + a11 * vector.getX1();
--- src/main/java/edu/ntnu/idatt2003/model/transformations/JuliaTransform.java
+++ src/main/java/edu/ntnu/idatt2003/model/transformations/JuliaTransform.java
@@ -3,16 +3,39 @@ package edu.ntnu.idatt2003.model.transformations;
 import edu.ntnu.idatt2003.model.basicLinalg.Vector2D;
 import edu.ntnu.idatt2003.model.basicLinalg.Complex;
 
+/**
+ * Class representing a 2D transformation of the form T(z) = sqrt(p * (z - c)).
+ * P is a complex number and c is a complex number.
+ *
+ * <p> The class includes a method to transform a 2D point using the transformation.
+ *
+ * @see Transform2D
+ * @see Complex
+ * @see Vector2D
+ * @since 0.1.0
+ */
 public class JuliaTransform extends Transform2D {
 
     private Complex point;
     private int sign;
 
+    /**
+     * Constructor for the JuliaTransform class
+     *
+     * @param inputPoint the complex number c
+     * @param inputSign  the sign of the transformation
+     */
     public JuliaTransform(Complex inputPoint, int inputSign) {
         this.point = inputPoint;
         this.sign = inputSign;
     }
 
+    /**
+     * Transform a 2D point using the Julia transformation.
+     *
+     * @param point the point to transform
+     * @return the transformed point as an instance of Vector2D
+     */
     @Override
     public Vector2D transform(Vector2D point) {
         Complex z = new Complex(point.getX0(), point.getX1());
