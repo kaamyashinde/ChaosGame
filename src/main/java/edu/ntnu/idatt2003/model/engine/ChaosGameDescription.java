7fe6579d52c33ba660fb667dfdea99b400e3b7fd
Kevin Dennis Mazali <kevindm@stud.ntnu>
2024-03-06 17:38:00 +0100

    refactor folder initilization

---
+++ src/main/java/edu/ntnu/idatt2003/model/engine/ChaosGameDescription.java
@@ -0,0 +1,4 @@
+package edu.ntnu.idatt2003.model.engine;
+
+public class ChaosGameDescription {
+}
---
+++ src/main/java/edu/ntnu/idatt2003/model/engine/ChaosGameFileHandler.java
@@ -0,0 +1,5 @@
+package edu.ntnu.idatt2003.model.engine;
+
+public class ChaosGameFileHandler {
+
+}
