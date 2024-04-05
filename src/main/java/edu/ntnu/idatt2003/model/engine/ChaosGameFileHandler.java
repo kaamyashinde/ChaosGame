d36f3d15fd53b2a7ed69e7a1a89f167b7816121d
Kevin Dennis Mazali <kevindm@stud.ntnu>
2024-04-05 18:14:58 +0200

    feat julia tranformation txt writing and commnets

--- src/main/java/edu/ntnu/idatt2003/model/engine/ChaosGameFileHandler.java
+++ src/main/java/edu/ntnu/idatt2003/model/engine/ChaosGameFileHandler.java
@@ -11,16 +11,11 @@ import edu.ntnu.idatt2003.model.transformations.Transform2D;
 import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
-// src/main/java/edu/ntnu/idatt2003/model/engine/test.txt
+// src/main/java/edu/ntnu/idatt2003/model/engine/testAffine.txt
 
 
-import java.io.*;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
-import java.util.Scanner;
 
 public class ChaosGameFileHandler {
 
@@ -32,60 +27,84 @@ public class ChaosGameFileHandler {
             // Check if we have transformations and write the type of the first one
             if (!chaosGameDescription.getTransforms().isEmpty()) {
                 String transformType = chaosGameDescription.getTransforms().get(0).getClass().getSimpleName();
-                writer.write(transformType);
+                writer.write(transformType + " # Type of transformation");
                 writer.newLine();
             }
 
             // Write the coordinates of the lower left corner
             Vector2D minCoords = chaosGameDescription.getMinCoords();
-            writer.write(minCoords.getX0() + "," + minCoords.getX1());
+            writer.write(minCoords.getX0() + "," + minCoords.getX1() + " # Lower left");
             writer.newLine();
 
             // Write the coordinates of the upper right corner
             Vector2D maxCoords = chaosGameDescription.getMaxCoords();
-            writer.write(maxCoords.getX0() + "," + maxCoords.getX1());
+            writer.write(maxCoords.getX0() + "," + maxCoords.getX1() + " # Upper right");
             writer.newLine();
 
             // Write the parameters of each transformation
+            int transformNumber = 1;
             for (Transform2D transform : chaosGameDescription.getTransforms()) {
                 if (transform instanceof AffineTransform2D) {
                     AffineTransform2D affine = (AffineTransform2D) transform;
                     Matrix2x2 matrix = affine.getMatrix();
                     Vector2D vector = affine.getVector();
-                    writer.write(matrix.getA00() + "," + matrix.getA01() + "," + matrix.getA10() + "," + matrix.getA11() + "," + vector.getX0() + "," + vector.getX1());
+                    writer.write(matrix.getA00() + "," + matrix.getA01() + "," + matrix.getA10() + "," + matrix.getA11() + "," + vector.getX0() + "," + vector.getX1() + " # " + transformNumber + "st transform");
                 } else if (transform instanceof JuliaTransform) {
                     JuliaTransform julia = (JuliaTransform) transform;
                     Complex point = julia.getPoint();
-                    writer.write(point.getReal() + "," + point.getImaginary());
+                    writer.write(point.getReal() + "," + point.getImaginary() + " # " + transformNumber + "st transform");
                 }
                 writer.newLine();
+                transformNumber++;
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-
-    public static void main(String[] args) {
-
-        //use the write to file method to write to a file
-
+    public static void initiateTransformationsAffine(){
         ChaosGameFileHandler chaosGameFileHandler = new ChaosGameFileHandler();
         Vector2D minCoords = new Vector2D(0, 0);
         Vector2D maxCoords = new Vector2D(1, 1);
         List<Transform2D> transforms = new ArrayList<>();
-        Matrix2x2 matrix = new Matrix2x2(1, 0, 0, 1);
-        Vector2D vector = new Vector2D(0, 0);
+        Matrix2x2 matrix = new Matrix2x2(0.5, 0, 0, 0.5); // er nå en rotasjonsmatrise, før med 1,0,0,1 var det en identitetsmatrise
 
-        //create a new affine transformation
+        // Create 3 transformations with slightly different vectors - //vet at opp til tre er riktig, med eksempelet vi har fått fra mappe tingz
+        for (int i = 0; i < 3; i++) {
+            Vector2D vector = new Vector2D(i * 0.25, 0); // Change the vector for each transformation
+            AffineTransform2D affine = new AffineTransform2D(matrix, vector);
+            transforms.add(affine);
+        }
 
-        AffineTransform2D affine = new AffineTransform2D(matrix, vector);
+        chaosGameFileHandler.writeToFile(new ChaosGameDescription(minCoords, maxCoords, transforms), "src/main/java/edu/ntnu/idatt2003/model/engine/testAffine.txt");
 
-        //add the affine transformation to the list of transformations
+    }
 
-        transforms.add(affine);
-        chaosGameFileHandler.writeToFile(new ChaosGameDescription(minCoords, maxCoords, transforms), "src/main/java/edu/ntnu/idatt2003/model/engine/test.txt");
+    //101 på hvordan JuliaTranformations fungerer
 
+    public static void iniateTransformationsJulia(){
+        ChaosGameFileHandler chaosGameFileHandler = new ChaosGameFileHandler();
+        Vector2D minCoords = new Vector2D(-1.6, -1);
+        Vector2D maxCoords = new Vector2D(1.6, 1);
+        List<Transform2D> transforms = new ArrayList<>();
+
+        // Create 3 JuliaTransforms with slightly different complex points
+        for (int i = 0; i < 3; i++) {
+            double realPart = -.74543 + i * 0.01; // Change the real part for each transformation
+            double imaginaryPart = .11301 + i * 0.01; // Change the imaginary part for each transformation
+            Complex point = new Complex(realPart, imaginaryPart);
+            JuliaTransform julia = new JuliaTransform(point, 1);
+            transforms.add(julia);
+        }
+
+        chaosGameFileHandler.writeToFile(new ChaosGameDescription(minCoords, maxCoords, transforms), "src/main/java/edu/ntnu/idatt2003/model/engine/testJulia.txt");
+    }
+
+
+    public static void main(String[] args) {
+
+        ChaosGameFileHandler.initiateTransformationsAffine();
+        ChaosGameFileHandler.iniateTransformationsJulia();
     }
 
 
