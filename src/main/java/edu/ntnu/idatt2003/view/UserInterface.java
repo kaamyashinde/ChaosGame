8e63a90dca8822209b04d446719cd0310ec39c1e
kaamyashinde <kaamyashinde@gmail.com>
2024-04-09 12:24:45 +0200

    implement a default config feature for sierpinksi triangle

--- src/main/java/edu/ntnu/idatt2003/view/UserInterface.java
+++ src/main/java/edu/ntnu/idatt2003/view/UserInterface.java
@@ -1,11 +1,18 @@
 package edu.ntnu.idatt2003.view;
 
+import edu.ntnu.idatt2003.model.basicLinalg.Matrix2x2;
+import edu.ntnu.idatt2003.model.basicLinalg.Vector2D;
 import edu.ntnu.idatt2003.model.engine.ChaosCanvas;
 import edu.ntnu.idatt2003.model.engine.ChaosGame;
 import edu.ntnu.idatt2003.model.engine.ChaosGameDescription;
 import edu.ntnu.idatt2003.model.engine.ChaosGameFileHandler;
+import edu.ntnu.idatt2003.model.transformations.AffineTransform2D;
+import edu.ntnu.idatt2003.model.transformations.Transform2D;
 
+import java.util.List;
 import java.util.Scanner;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class UserInterface {
     /**
@@ -24,6 +31,10 @@ public class UserInterface {
      * Constant corresponding to the print fractal action.
      */
     private static final int PRINT_FRACTAL = 4;
+    /**
+     * Constant corresponding to the default configuration.
+     */
+    private static final int DEFAULT_CONFIG = 5;
     /**
      * A Scanner object to read input from the console.
      */
@@ -95,6 +106,7 @@ public class UserInterface {
                         2. Write description to a file
                         3. Run a given number of iterations
                         4. Print the ASCII-fractal to the console
+                        5. Run a default configuration for the Sierpinski triangle
                         --------------------------
                         What would u like to do? (Enter a number between 0 and 4).
                         """);
@@ -112,6 +124,7 @@ public class UserInterface {
             case WRITE_TO_FILE -> writeToFile();
             case RUN_ITERATIONS -> runIterations();
             case PRINT_FRACTAL -> printFractal();
+            case DEFAULT_CONFIG -> runDefaultConfig();
             default -> System.out.println("Invalid input, please try again.");
         }
     }
@@ -184,5 +197,71 @@ public class UserInterface {
                 System.out.println();
             }
         }
+
+    }
+    private static void runDefaultConfig(){
+        String filePath = "src/main/java/edu/ntnu/idatt2003/model/engine/DefaultConfigTriangle.txt";
+        chaosGameDescription = ChaosGameFileHandler.readFromFile(filePath);
+        if (chaosGameDescription == null){
+            System.out.println("Could not read the default configuration file.");
+            return;
+        }
+        chaosGame = new ChaosGame(chaosGameDescription, 60, 20);
+
+        System.out.println("Enter the number of iterations you want to run: ");
+        int iterations = input.nextInt();
+        chaosGame.runSteps(iterations);
+        System.out.println("Successfully ran " + iterations + " iterations.");
+        System.out.println("Here is the ASCII-fractal: \n");
+
+        ChaosCanvas canvas = chaosGame.getCanvas();
+        int[][] canvasArray = canvas.getCanvasArray();
+        int numberOfPixels = 0;
+        for (int i = 0; i < canvasArray.length; i++) {
+            for (int j = 0; j < canvasArray[i].length; j++) {
+                System.out.print(canvasArray[i][j] == 0 ? " " : "*");
+                if (canvasArray[i][j] == 1){
+                    numberOfPixels++;
+
+                }
+
+            }
+            System.out.println(" ");
+        }
+    }
+    public static void main(String[] args) {
+        ChaosGame chaosGame;
+        ChaosGameDescription chaosGameDescription;
+        Vector2D minCoords;
+        Vector2D maxCoords;
+        List<Transform2D> transforms;
+
+        minCoords = new Vector2D(0, 0);
+        maxCoords = new Vector2D(1, 1);
+        transforms = List.of(
+                new AffineTransform2D(new Matrix2x2(0.5, 0, 0, 0.5), new Vector2D(0, 0)),
+                new AffineTransform2D(new Matrix2x2(0.5, 0, 0, 0.5), new Vector2D(0.25, 0.5)),
+                new AffineTransform2D(new Matrix2x2(0.5, 0, 0, 0.5), new Vector2D(0.5, 0)));
+        chaosGameDescription = new ChaosGameDescription(minCoords, maxCoords, transforms);
+        chaosGame = new ChaosGame(chaosGameDescription, 60, 20);
+
+        chaosGame.runSteps(100);
+
+        ChaosCanvas canvas = chaosGame.getCanvas();
+        int[][] canvasArray = canvas.getCanvasArray();
+        int numberOfPixels = 0;
+        for (int i = 0; i < canvasArray.length; i++) {
+            for (int j = 0; j < canvasArray[i].length; j++) {
+                System.out.print(canvasArray[i][j] == 0 ? " " : "*");
+                if (canvasArray[i][j] == 1){
+                    numberOfPixels++;
+
+                }
+
+            }
+            System.out.println(" ");
+        }
+
+
     }
 }
