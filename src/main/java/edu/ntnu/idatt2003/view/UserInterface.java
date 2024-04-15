3cd084cb7c856799dcc33db370446b56f7d9bbde
kaamyashinde <kaamyashinde@gmail.com>
2024-04-15 20:28:09 +0200

    implement presets option in writeToFile method ui

--- src/main/java/edu/ntnu/idatt2003/view/UserInterface.java
+++ src/main/java/edu/ntnu/idatt2003/view/UserInterface.java
@@ -38,10 +38,6 @@ public class UserInterface {
      */
     private static final int PRINT_FRACTAL = 4;
     /**
-     * Constant corresponding to the default configuration.
-     */
-    private static final int PRINT_FERN = 5;
-    /**
      * A Scanner object to read input from the console.
      */
     private static final Scanner input = new Scanner(System.in);
@@ -68,6 +64,10 @@ public class UserInterface {
     private UserInterface() {
     }
 
+    /**
+     * Method to launch the UserInterface.
+     * It runs the init and start methods.
+     */
     public static void launch() {
         init();
         start();
@@ -103,6 +103,13 @@ public class UserInterface {
 
     /**
      * Method to show the menu and take the user input.
+     * <ol>
+     *     <li> The user is able to read a description from a file. This description is printed out and the configurations are saved in the variable {@code chaosGameDescription}.</li>
+     *     <li> The user is able to write a description to a file. If the file does not exist, a new one will be created. The description can either be generated automatically or entered manually.</li>
+     *     <li> The user can run a given number of iterations for a specific chaosGame description.</li>
+     *     <li> The result from running the ChaosGame is printed out to the console along with which description it belongs to.</li>
+     *     <li> The user is provided with a list of presets whose description can be read and written to a file that the user chooses. </li>
+     * </ol>
      *
      * @return the integer corresponding the action the user wants to perform.
      */
@@ -115,7 +122,6 @@ public class UserInterface {
                         2. Write description to a file
                         3. Run a given number of iterations
                         4. Print the fractal to the console
-                        5. Print the Barnsley fern
                         --------------------------
                         What would u like to do? (Enter a number between 0 and 5).
                         """);
@@ -133,7 +139,6 @@ public class UserInterface {
             case WRITE_TO_FILE -> writeToFile();
             case RUN_ITERATIONS -> runIterations();
             case PRINT_FRACTAL -> printFractal();
-            case PRINT_FERN -> printFern();
             default -> System.out.println("-> Invalid input, please try again.");
         }
     }
@@ -185,36 +190,50 @@ public class UserInterface {
         String fileName = input.next();
 
         String filePath = "src/main/java/edu/ntnu/idatt2003/resources/" + fileName;
-
-        System.out.println("Which kind of transformation do you want to generate? 1: Affine, 2: Julia");
-        int choice = input.nextInt();
-        if (choice == 1) {
-            chaosGameDescription = initiateTransformationAffine();
-        } else if (choice == 2) {
-          chaosGameDescription = initiateTransformationJulia();
+        System.out.println("Do u want to generate your own transformation or use a preset? 1: ");
+        System.out.println("""
+                1. Own
+                2. Preset
+                """);
+        int ownOrPreset = input.nextInt();
+        if (ownOrPreset == 1) {
+            System.out.println("Which kind of transformation do you want to generate? 1: Affine, 2: Julia");
+            int choice = input.nextInt();
+            if (choice == 1) {
+                chaosGameDescription = initiateTransformationAffine();
+            } else if (choice == 2) {
+                chaosGameDescription = initiateTransformationJulia();
+            } else {
+                System.out.println("-> Invalid input, please try again.");
+            }
+        } else if (ownOrPreset == 2) {
+            runPreset();
         } else {
             System.out.println("-> Invalid input, please try again.");
         }
+
         ChaosGameFileHandler.writeToFile(chaosGameDescription, filePath);
 
     }
+
     /**
      * Method to initiate the transformation for the Affine set.
      * <p>
-     *   The user is prompted to enter the coordinates for the transformations.
-     *   Then the user is asked whether they want to create the transformations manually or automatically.
-     *   <ol>
-     *       <li>
-     *       If the user chooses to create the transformations manually, they are prompted to enter the values for the complex number for each transformation.
-     *       </li>
-     *       <li>
-     *           If the user chooses to create the transformations automatically, they are prompted to enter the number of transformations to generate.
-     *           </li>
-     *   </ol>
+     * The user is prompted to enter the coordinates for the transformations.
+     * Then the user is asked whether they want to create the transformations manually or automatically.
+     * <ol>
+     *     <li>
+     *     If the user chooses to create the transformations manually, they are prompted to enter the values for the complex number for each transformation.
+     *     </li>
+     *     <li>
+     *         If the user chooses to create the transformations automatically, they are prompted to enter the number of transformations to generate.
+     *         </li>
+     * </ol>
+     *
      * @return an object of the ChaosGameDescription class containing the coordinates and transformations.
      */
 
-    private static ChaosGameDescription initiateTransformationAffine(){
+    private static ChaosGameDescription initiateTransformationAffine() {
         Vector2D[] coords = userInputForCoordsOfTransformations();
 
         System.out.println("How do you want to generate the affine transformations? 1: Manually, 2: Automatically");
@@ -226,13 +245,13 @@ public class UserInterface {
             System.out.println("How many transformations do you want to generate?");
             int numberOfTransformations = input.nextInt();
 
-            for (int i = 0; i < numberOfTransformations; i++){
+            for (int i = 0; i < numberOfTransformations; i++) {
                 System.out.println("Enter the values for the matrix for transformation " + (i + 1) + ". Separate the values with a space.");
                 double matrix00 = input.nextDouble();
                 double matrix01 = input.nextDouble();
                 double matrix10 = input.nextDouble();
                 double matrix11 = input.nextDouble();
-               Matrix2x2 matrix = new Matrix2x2(matrix00, matrix01, matrix10, matrix11);
+                Matrix2x2 matrix = new Matrix2x2(matrix00, matrix01, matrix10, matrix11);
 
                 System.out.println("Enter the values for the vector for transformation " + (i + 1) + ". Separate the values with a space.");
                 double vector0 = input.nextDouble();
@@ -259,19 +278,20 @@ public class UserInterface {
     /**
      * Method to initiate the transformation for the Julia set.
      * <p>
-     *   The user is prompted to enter the coordinates for the transformations.
-     *   Then the user is asked whether they want to create the transformations manually or automatically.
-     *   <ol>
-     *       <li>
-     *       If the user chooses to create the transformations manually, they are prompted to enter the values for the complex number for each transformation.
-     *       </li>
-     *       <li>
-     *           If the user chooses to create the transformations automatically, they are prompted to enter the number of transformations to generate.
- *           </li>
-     *   </ol>
+     * The user is prompted to enter the coordinates for the transformations.
+     * Then the user is asked whether they want to create the transformations manually or automatically.
+     * <ol>
+     *     <li>
+     *     If the user chooses to create the transformations manually, they are prompted to enter the values for the complex number for each transformation.
+     *     </li>
+     *     <li>
+     *         If the user chooses to create the transformations automatically, they are prompted to enter the number of transformations to generate.
+     *         </li>
+     * </ol>
+     *
      * @return an object of the ChaosGameDescription class containing the coordinates and transformations.
      */
-    private static ChaosGameDescription initiateTransformationJulia(){
+    private static ChaosGameDescription initiateTransformationJulia() {
         Vector2D[] coords = userInputForCoordsOfTransformations();
 
         System.out.println("How do you want to generate the Julia transformations? 1: Manually, 2: Automatically");
@@ -283,7 +303,7 @@ public class UserInterface {
             System.out.println("How many transformations do you want to add");
             int numberOfTransformations = input.nextInt();
 
-            for (int i = 0; i < numberOfTransformations; i++){
+            for (int i = 0; i < numberOfTransformations; i++) {
                 System.out.println("Enter the values for the complex number for transformation " + (i + 1) + ". Separate the values with a space.");
                 double realPart = input.nextDouble();
                 double imaginaryPart = input.nextDouble();
@@ -305,9 +325,10 @@ public class UserInterface {
     /**
      * Method that prompts the user for the minimum and maximum coordinates of a transformation.
      * This is then used to create an array of Vector2D objects that is returned.
+     *
      * @return an array of Vector2D objects containing the min and max coordinates.
      */
-    private static Vector2D[] userInputForCoordsOfTransformations(){
+    private static Vector2D[] userInputForCoordsOfTransformations() {
         System.out.println("What are the minimum coordinates for the canvas? Separate the x and y values with a space.");
         double minCoordsX0 = input.nextDouble();
         double minCoordsX1 = input.nextDouble();
@@ -317,13 +338,11 @@ public class UserInterface {
         return InitateTransformations.coordsForTransformation(minCoordsX0, minCoordsX1, maxCoordsX0, maxCoordsX1);
     }
 
-
-
     /**
      * Method to run a specific number of iterations
      * <p>
-     *    The user can decide between entering values for the width and height of the canvas or use the default configuration.
-     *    Then the user is prompted to enter the number of iterations to run. A message is printed after the iterations are run.
+     * The user can decide between entering values for the width and height of the canvas or use the default configuration.
+     * Then the user is prompted to enter the number of iterations to run. A message is printed after the iterations are run.
      * </p>
      */
     private static void runIterations() {
@@ -359,8 +378,8 @@ public class UserInterface {
     /**
      * Method to print the ASCII-fractal to the console.
      * <p>
-     *     If the chaosGame object is empty, the user is prompted to run the chaos game first.
-     *     The chaosGame object might be empty if the user has not run the runIterations method.
+     * If the chaosGame object is empty, the user is prompted to run the chaos game first.
+     * The chaosGame object might be empty if the user has not run the runIterations method.
      * </p>
      */
     private static void printFractal() {
@@ -373,44 +392,95 @@ public class UserInterface {
         System.out.println("--------------------------------------------------------");
         ChaosCanvas canvas = chaosGame.getCanvas();
         int[][] canvasArray = canvas.getCanvasArray();
-        for (int i = 0; i < canvasArray.length; i++) {
-            for (int j = 0; j < canvasArray[i].length; j++) {
-                System.out.print(canvasArray[i][j] == 0 ? " " : "*");
+        for (int[] yValues : canvasArray) {
+            for (int xValues : yValues) {
+                System.out.print(xValues == 0 ? " " : "*");
             }
             System.out.println(" ");
         }
     }
 
     /**
-     * Method that is uesd to test whether the Bransley fern is printed correctly.
+     * A case switch that handles the printing of the desired preset.
      */
-    private static void printFern(){
-        Vector2D minCoords = new Vector2D(-2.65, 0);
-        Vector2D maxCoords = new Vector2D(2.65, 10);
+    private static void runPreset() {
+        int choice = initiatePreset();
+        switch (choice) {
+            case 1 -> chaosGameDescription = defaultSierpinskiTriangle();
+            case 2 -> chaosGameDescription = defaultBarnsleyFern();
+            case 3 -> chaosGameDescription = defaultJuliaSet();
+            default -> System.out.println("-> Invalid input, please try again.");
+        }
+    }
+
+    /**
+     * Method that prompts the user to choose a preset from the following list:
+     * <ol>
+     *     <li>Sierpinski triangle</li>
+     *     <li>Barnsley fern</li>
+     *     <li>Julia set</li>
+     * </ol>
+     *
+     * @return Returns an integer corresponding to the preset chosen.
+     */
+
+    private static int initiatePreset() {
+        System.out.println("Which preset would you like to use? 1:");
+        System.out.println("""
+                1. Sierpinski triangle
+                2. Barnsley fern
+                3. Julia set
+                """);
+        return input.nextInt();
+    }
+
+
+    /**
+     * The method initiates the Sierpinski triangle preset.
+     *
+     * @return an object of the ChaosGameDescription class containing the coordinates and transformations for the Sierpinski triangle.
+     */
+    private static ChaosGameDescription defaultSierpinskiTriangle() {
+        Vector2D[] coords = InitateTransformations.coordsForTransformation(0, 0, 1, 1);
+        List<Transform2D> transforms = InitateTransformations.listOfTransformationsAffineGeneration(3, 0.5, 0, 0, 0.5);
+        return new ChaosGameDescription(coords[0], coords[1], transforms);
+    }
+
+    /**
+     * The method initiates the Barnsley fern preset.
+     *
+     * @return an object of the ChaosGameDescription class containing the coordinates and transformations for the Barnsley fern.
+     */
+    private static ChaosGameDescription defaultBarnsleyFern() {
+        Vector2D[] coords = InitateTransformations.coordsForTransformation(-2.65, 0, 2.65, 10);
         Transform2D transform1 = new AffineTransform2D(
-                new Matrix2x2(0,0,0,0.16),
-                new Vector2D(0,0)
+                new Matrix2x2(0, 0, 0, 0.16),
+                new Vector2D(0, 0)
         );
         Transform2D transform2 = new AffineTransform2D(
-                new Matrix2x2(0.85,0.04,-0.04,0.85),
-                new Vector2D(0,1.6)
+                new Matrix2x2(0.85, 0.04, -0.04, 0.85),
+                new Vector2D(0, 1.6)
         );
         Transform2D transform3 = new AffineTransform2D(
-                new Matrix2x2(0.2,-0.26,0.23,0.22),
-                new Vector2D(0,1.6)
+                new Matrix2x2(0.2, -0.26, 0.23, 0.22),
+                new Vector2D(0, 1.6)
         );
         Transform2D transform4 = new AffineTransform2D(
-                new Matrix2x2(-0.15,0.28,0.26,0.24),
-                new Vector2D(0,0.44)
+                new Matrix2x2(-0.15, 0.28, 0.26, 0.24),
+                new Vector2D(0, 0.44)
         );
-        ChaosGameDescription chaosGameDescription = new ChaosGameDescription(
-                minCoords,
-                maxCoords, List.of(transform1, transform2, transform3, transform4)
-        );
-        chaosGame = new ChaosGame(chaosGameDescription, 200, 100);
-        System.out.println("Enter the number of iterations you want to run: ");
-        int n = input.nextInt();
-        chaosGame.runSteps(n);
-        printFractal();
+        List<Transform2D> transforms = List.of(transform1, transform2, transform3, transform4);
+        return new ChaosGameDescription(coords[0], coords[1], transforms);
+    }
+
+    /**
+     * The method initiates the Julia set preset.
+     *
+     * @return an object of the ChaosGameDescription class containing the coordinates and transformations for the Julia set.
+     */
+    private static ChaosGameDescription defaultJuliaSet() {
+        Vector2D[] coords = InitateTransformations.coordsForTransformation(-1.6, -1.0, 1.6, 1.0);
+        List<Transform2D> transforms = InitateTransformations.listOfTransformationsJuliaGeneration(10);
+        return new ChaosGameDescription(coords[0], coords[1], transforms);
     }
 }
