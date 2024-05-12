7364d3f6e394f51695576b5f073a57f7b910939a
kaamyashinde <kaamyashinde@gmail.com>
2024-05-12 14:50:04 +0200

    merge controller and julia scene to implement presets

--- src/main/java/edu/ntnu/idatt2003/view/JuliaScene.java
+++ src/main/java/edu/ntnu/idatt2003/view/JuliaScene.java
@@ -3,9 +3,12 @@ package edu.ntnu.idatt2003.view;
 
 import edu.ntnu.idatt2003.controller.Controller;
 import edu.ntnu.idatt2003.model.ChaosGameObserver;
+import edu.ntnu.idatt2003.model.engine.ChaosGame;
+import edu.ntnu.idatt2003.model.engine.ChaosGameDescription;
 import javafx.application.Application;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
@@ -16,12 +19,16 @@ import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
+import java.util.Stack;
+
 public class JuliaScene extends Application implements ChaosGameObserver {
   private static final String FILE_PATH = "src/main/resources/";
   AnchorPane layout;
   GraphicsContext gc;
   Controller controller;
-
+  ChaosGame game;
+  Button addThousandPixelsButton;
+  ChaosGameDescription chaosGameDescription;
   public static void main(String[] args) {
     launch(args);
   }
@@ -37,6 +44,7 @@ public class JuliaScene extends Application implements ChaosGameObserver {
     root.getChildren().addAll(navigationHBox(), titleHBox(), bodyHBox(), footerHBox());
     root.getChildren().stream().filter(node -> node instanceof HBox).forEach(node -> ((HBox) node).prefWidthProperty().bind(root.widthProperty()));
 
+
     Scene scene = new Scene(layout, 1000, 700);
     primaryStage.setScene(scene);
     primaryStage.show();
@@ -51,6 +59,64 @@ public class JuliaScene extends Application implements ChaosGameObserver {
   @Override
   public void updateRemovePixel(double X0, double X1) {
     gc.clearRect(X0, X1, 1, 1);
+  }
+  /**
+   * Method that uses the controller to gain access to the chaos game.
+   *
+   */
+  private StackPane gamePaneCanvas() {
+    updateChaosGameObject(controller.readChaosGameDescriptionFromFile("Affine.txt"));
+    Canvas canvas = new Canvas(1000, 700);
+    gc = canvas.getGraphicsContext2D();
+    StackPane chaosGamePane = new StackPane();
+    chaosGamePane.getChildren().add(canvas);
+
+    return chaosGamePane;
+  }
+
+  /**
+   * Method that creates the buttons for the different fractals and returns them.
+   * @param fractalName The name of the fractal.
+   * @return The button for the fractal.
+   */
+
+  public Button createPresetFractalButton(String fractalName){
+    Button button = new Button(fractalName);
+    button.setOnAction(e ->{
+      switch (fractalName){
+        case "Julia":
+          chaosGameDescription = controller.readChaosGameDescriptionFromFile("Julia.txt");
+          updateChaosGameObject(chaosGameDescription);
+          game.getCanvas().clear();
+          break;
+        case "Barnsley":
+          chaosGameDescription = controller.readChaosGameDescriptionFromFile("Barnsley.txt");
+          updateChaosGameObject(chaosGameDescription);
+          game.getCanvas().clear();
+
+          break;
+        case "Sierpinski":
+          chaosGameDescription = controller.readChaosGameDescriptionFromFile("Affine.txt");
+          updateChaosGameObject(chaosGameDescription);
+          game.getCanvas().clear();
+
+          break;
+        default:
+          chaosGameDescription = controller.readChaosGameDescriptionFromFile("Default.txt");
+          updateChaosGameObject(chaosGameDescription);
+
+          break;
+      }
+    });
+    return button;
+  }
+  /**
+   * Update the chaosGame object.
+   */
+  private void updateChaosGameObject(ChaosGameDescription input){
+    game = new ChaosGame(input, 1000, 1000);
+    game.addObserver(this);
+    game.getCanvas().addObserver(this);
   }
 
   /**
@@ -88,12 +154,13 @@ public class JuliaScene extends Application implements ChaosGameObserver {
   private HBox bodyHBox() {
     HBox bodyRow = new HBox();
     VBox leftBodyRow = new VBox();
-    StackPane chaosGamePane = new StackPane();
+    StackPane chaosGamePane = gamePaneCanvas();
+
     VBox rightBodyRow = new VBox();
     //startGame();
-    rightBodyRow.getChildren().add(controller.createPresetFractalButton("Affine"));
-    rightBodyRow.getChildren().add(controller.createPresetFractalButton("Barnsley"));
-    rightBodyRow.getChildren().add(controller.createPresetFractalButton("Sierpinski"));
+    rightBodyRow.getChildren().add(createPresetFractalButton("Julia"));
+    rightBodyRow.getChildren().add(createPresetFractalButton("Barnsley"));
+    rightBodyRow.getChildren().add(createPresetFractalButton("Sierpinski"));
     bodyRow.getChildren().addAll(leftBodyRow, chaosGamePane, rightBodyRow);
     bodyRow.setAlignment(Pos.CENTER);
     return bodyRow;
@@ -104,7 +171,14 @@ public class JuliaScene extends Application implements ChaosGameObserver {
    */
   private HBox footerHBox() {
     HBox footerRow = new HBox();
-    Button addThousandPixelsButton = new Button("Add 10000");
+    addThousandPixelsButton = new Button("Add 10000");
+    addThousandPixelsButton.setOnAction(e ->
+    {
+      game.runSteps(10000);
+      game.actionToNotifyObserversAbout();
+
+    });
+
     Button clearCanvasButton = new Button("Clear Canvas");
     footerRow.getChildren().addAll(addThousandPixelsButton, clearCanvasButton);
     footerRow.setAlignment(Pos.CENTER);
