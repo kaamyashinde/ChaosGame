fab154070704e44745af1db026290331ae3986b9
kaamyashinde <kaamyashinde@gmail.com>
2024-05-14 18:58:39 +0200

    create controller class for list generations

--- src/main/java/edu/ntnu/idatt2003/controller/TextFieldsController.java
+++ src/main/java/edu/ntnu/idatt2003/controller/TextFieldsController.java
@@ -1,2 +1,63 @@
-package edu.ntnu.idatt2003.controller;public class TextFieldsController {
+package edu.ntnu.idatt2003.controller;
+
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+import java.util.List;
+
+/**
+ * The controller for the text fields in the GUI.
+ * It is responsible for creating different types of lists for the text fields.
+ */
+
+public class TextFieldsController {
+  /**
+   * Method that creates a list containing the max and min coordinates text fields.
+   * @return a list containing the max and min coordinates text fields.
+   */
+  public List<TextField> maxAndMinCoordsTextFieldsList() {
+
+    TextField minCoordsX0 = new TextField();
+    TextField minCoordsX1 = new TextField();
+    TextField maxCoordsX0 = new TextField();
+    TextField maxCoordsX1 = new TextField();
+
+    minCoordsX0.setPromptText("Min X0");
+    minCoordsX1.setPromptText("Min X1");
+    maxCoordsX0.setPromptText("Max X0");
+    maxCoordsX1.setPromptText("Max X1");
+
+    return List.of(minCoordsX0, minCoordsX1, maxCoordsX0, maxCoordsX1);
+  }
+
+  /**
+   * Method that creates a list containing the buttons used to control the viewing of the Affine transformations.
+   * @return a list containing the buttons used to control the viewing of the Affine transformations.
+   */
+  public List<Button> affineTransformationButtonsList() {
+    Button previousTransformation = new Button("Previous Transformation");
+    Button nextTransformation = new Button("Next Transformation");
+    return List.of(previousTransformation, nextTransformation);
+  }
+  /**
+   * Method that creates a list containing the matrix and vector for a specific affine transformation.
+   * @return a list containing the matrix and vector for a specific affine transformation.
+   */
+  public List<TextField> affineTransformationTextFieldsList() {
+    TextField matrixA00 = new TextField();
+    TextField matrixA01 = new TextField();
+    TextField matrixA10 = new TextField();
+    TextField matrixA11 = new TextField();
+    TextField vectorB0 = new TextField();
+    TextField vectorB1 = new TextField();
+    matrixA00.setPromptText("Matrix A00");
+    matrixA01.setPromptText("Matrix A01");
+    matrixA10.setPromptText("Matrix A10");
+    matrixA11.setPromptText("Matrix A11");
+    vectorB0.setPromptText("Vector B0");
+    vectorB1.setPromptText("Vector B1");
+
+    return List.of(matrixA00, matrixA01, matrixA10, matrixA11, vectorB0, vectorB1);
+  }
+
 }
