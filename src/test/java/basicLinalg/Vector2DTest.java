efd71439335ce7c4a7492a19e80c96ba6dd6f02b
Kevin Dennis Mazali <kevindm@stud.ntnu>
2024-05-15 10:07:58 +0200

    Test - negative tests for the BasicLinAlg package

--- src/test/java/basicLinalg/Vector2DTest.java
+++ src/test/java/basicLinalg/Vector2DTest.java
@@ -6,27 +6,20 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
-/**
- * JUnit tests for the class Vector2D. The tests are for the methods that add and subtract vectors.
- */
 class Vector2DTest {
 
     private Vector2D v1;
     private Vector2D v2;
 
-    /**
-     * Creates instances of the object Vector2D for testing.
-     */
     @BeforeEach
     public void setUp() {
         v1 = new Vector2D(1, 2);
         v2 = new Vector2D(3, 4);
     }
 
-    /**
-     * Testing the add method in the Vector2D class.
-     */
+//positive test
     @Test
     @DisplayName("Add two vectors")
     void add() {
@@ -35,9 +28,6 @@ class Vector2DTest {
         assertEquals(6, v3.getX1());
     }
 
-    /**
-     * Testing the subtract method in the Vector2D class.
-     */
     @Test
     @DisplayName("Subtract two vectors")
     void subtract() {
@@ -46,4 +36,12 @@ class Vector2DTest {
         assertEquals(-2, v3.getX1());
     }
 
+    //negative test
+    @Test
+    @DisplayName("Invalid vector values")
+    void invalidVectorValuesTest(){
+        assertThrows(IllegalArgumentException.class, () -> {
+            Vector2D invalidVector = new Vector2D(-600, 2); // x0 value is out of range
+        });
+    }
 }
