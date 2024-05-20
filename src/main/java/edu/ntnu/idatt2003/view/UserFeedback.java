2d39e2bfb453147743dcc9cd367e9db25fa84bec
kaamyashinde <kaamyashinde@gmail.com>
2024-05-20 21:12:42 +0200

    implement user manual from welcome popup

--- src/main/java/edu/ntnu/idatt2003/view/UserFeedback.java
+++ src/main/java/edu/ntnu/idatt2003/view/UserFeedback.java
@@ -15,19 +15,22 @@ import static edu.ntnu.idatt2003.view.PopupScene.*;
 /**
  * This class is responsible for creating the pop-up windows for the user feedback.
  * The class contains methods that display error messages and welcome messages.
+ *
  * @author Kaamya Shinde
- * @version 0.3
+ * @version 0.4
  * @since 0.3.2
  */
 public class UserFeedback {
   private static final GameController gameController = GameController.getInstance();
+
   /**
    * Method that styles the text area by adding the content style to it.
+   *
    * @param inputMessage The message that is to be displayed.
    * @return textArea The text area that is to be displayed.
    */
 
-  private static TextArea getStyledTextArea(String inputMessage){
+  private static TextArea getStyledTextArea(String inputMessage) {
     TextArea textArea = new TextArea(inputMessage);
     textArea.getStyleClass().add("content");
     textArea.setWrapText(true);
@@ -36,10 +39,11 @@ public class UserFeedback {
 
   /**
    * Method that displays an error message to the user.
-   * @param message The message that is to be displayed.
+   *
+   * @param message   The message that is to be displayed.
    * @param errorDesc The description of the error along with description on how to avoid it.
    */
-  public static void displayError(String message, String errorDesc){
+  public static void displayError(String message, String errorDesc) {
     System.out.println(message);
     Stage popupStage = createPopupStage("Error", null);
     VBox popupLayout = createPopupLayout(popupStage);
@@ -60,7 +64,7 @@ public class UserFeedback {
    * This one is displayed when the user restarts the game.
    * The user is provided with the option to start where they left off.
    */
-  public static void welcomeBackMessage(Stage primaryStage, ChaosGameObserver observer){
+  public static void welcomeBackMessage(Stage primaryStage, ChaosGameObserver observer) {
 
 
     Stage popupStage = createPopupStage("Welcome back!", primaryStage);
@@ -95,7 +99,7 @@ public class UserFeedback {
     buttons.setAlignment(javafx.geometry.Pos.CENTER);
     buttons.getChildren().addAll(startNewGame, continueGame);
     VBox container = new VBox();
-    container.getChildren().addAll(headingOne,headingTwo, TextArea1,buttons);
+    container.getChildren().addAll(headingOne, headingTwo, TextArea1, buttons);
     popupLayout.getChildren().add(container);
     dimBackground(gameController.getPrimaryStage(), popupStage);
 
@@ -110,7 +114,7 @@ public class UserFeedback {
    * The user is provided with the option to start a new game.
    * The user is also informed about how the chaos game works, and how to play it.
    */
-  public static void welcomeMessage(Stage primaryStage, ChaosGameObserver observer){
+  public static void welcomeMessage(Stage primaryStage, ChaosGameObserver observer) {
     Stage popupStage = createPopupStage("Welcome to the Chaos Game!", primaryStage);
     VBox popupLayout = createPopupLayout(popupStage);
     TextField headingOne = new TextField("Welcome to the ");
@@ -127,13 +131,17 @@ public class UserFeedback {
     startNewGame.setOnAction(e -> {
       popupStage.close();
     });
-  Button whatCanIDo = new Button("What can I do?");
-  whatCanIDo.setAlignment(javafx.geometry.Pos.CENTER);
+    Button whatCanIDo = new Button("What can I do?");
+    whatCanIDo.setAlignment(javafx.geometry.Pos.CENTER);
+    whatCanIDo.setOnAction(e->{
+      popupStage.close();
+      displayUserManual();
+    });
     HBox buttons = new HBox();
     buttons.setAlignment(javafx.geometry.Pos.CENTER);
     buttons.getChildren().addAll(startNewGame, whatCanIDo);
     VBox container = new VBox();
-    container.getChildren().addAll(headingOne,headingTwo, TextArea1,buttons);
+    container.getChildren().addAll(headingOne, headingTwo, TextArea1, buttons);
     popupLayout.getChildren().add(container);
     dimBackground(gameController.getPrimaryStage(), popupStage);
 
@@ -146,7 +154,7 @@ public class UserFeedback {
    * This method is called when the user chooses to continue the game.
    * The user is informed about how to continue the game.
    */
-  private static void informUserAboutContinuingGame(){
+  private static void informUserAboutContinuingGame() {
     Stage popupStage = createPopupStage("Continuing the game", gameController.getPrimaryStage());
     VBox popupLayout = createPopupLayout(popupStage);
 
@@ -169,6 +177,73 @@ public class UserFeedback {
 
   }
 
+  /**
+   * Provide the user with an user manual when the user clicks on the "What can I do?" button.
+   * This includes of the following options:
+   * <ol>
+   *   <li> Create a new empty file of either Julia or Affine type. The default file creation is set to julia. Clicking on the "Switch to Affine" button gives the user an option to enter the number of transformations.</li>
+   *   <li>Read description from a file by selecting one from the drop down list. The user has to click on the "update chaos game" button in order to register the new configuration.</li>
+   * <li>Choose a preset fractal. The  user can choose between three different fractals that have been loaded at application start. This is run by adjusting the number of iterations the user wants and running them by clicking on the "run iterations" button.</li>
+   * <li>Edit the value of the configuration. There are two ways to do this :
+   *      <ol>
+   *        <li>Edit Current Game Description. The user updates the value of the configuration that is currently being used.</li>
+   *        <li>Edit Selected Description. The user can select a file to read from the drop down list and then edit the selected file. The user does not need to click on the "update chaos game" button in order for the "edit selected description" button to work. If the user chooses to update the chaos game after reading from the file, they can also use the "edit current description" button to update the configuration.</li>
+   *      </ol>
+   *      After doing so, the user has to enter the name of the file they want to save the description to. It is possible to create a new file, by writing a new name in the input, or the user can replace the existing file by writing the name of the file.
+   *      In order to save the file, the user has to click on the "save description" button. Otherwise, all of the changes will be lost.
+   * </li>
+   * <li>The run steps iterations runs the game for the given number of steps. It is possible to clear out the canvas by clicking on the "clear canvas" button.</li>
+   * </ol>
+   */
+  public static void displayUserManual() {
+    Stage popupStage = createPopupStage("User Manual", gameController.getPrimaryStage());
+    VBox popupLayout = createPopupLayout(popupStage);
+
+    VBox container = new VBox();
+
+    TextField heading1 = new TextField("1. Create a new empty file");
+    heading1.setEditable(false);
+    heading1.getStyleClass().add("user-manual-title");
+    TextArea instructions1 = getStyledTextArea("Create a new empty file of either Julia or Affine type. The default file creation is set to julia. Clicking on the \"Switch to Affine\" button gives the user an option to enter the number of transformations.");
+
+    TextField heading2 = new TextField("2. Read description from a file");
+    heading2.setEditable(false);
+    heading2.getStyleClass().add("user-manual-title");
+    TextArea instructions2 = getStyledTextArea("Read description from a file by selecting one from the drop down list. The user has to click on the \"update chaos game\" button in order to register the new configuration.");
+
+    TextField heading3 = new TextField("3. Choose a preset fractal");
+    heading3.setEditable(false);
+    heading3.getStyleClass().add("user-manual-title");
+    TextArea instructions3 = getStyledTextArea("The user can choose between three different fractals that have been loaded at application start. This is run by adjusting the number of iterations the user wants and running them by clicking on the \"run iterations\" button.");
+
+    TextField heading4 = new TextField("4. Edit the value of the configuration");
+    heading4.setEditable(false);
+    heading4.getStyleClass().add("user-manual-title");
+    TextArea instructions4 = getStyledTextArea("There are two ways to do this :\n" +
+        "    a. Edit Current Game Description. The user updates the value of the configuration that is currently being used.\n" +
+        "    b. Edit Selected Description. The user can select a file to read from the drop down list and then edit the selected file. The user does not need to click on the \"update chaos game\" button in order for the \"edit selected description\" button to work. If the user chooses to update the chaos game after reading from the file, they can also use the \"edit current description\" button to update the configuration.\n" +
+        "    After doing so, the user has to enter the name of the file they want to save the description to. It is possible to create a new file, by writing a new name in the input, or the user can replace the existing file by writing the name of the file.\n" +
+        "    In order to save the file, the user has to click on the \"save description\" button. Otherwise, all of the changes will be lost.");
+
+    TextField heading5 = new TextField("5. The run steps iterations");
+    heading5.setEditable(false);
+    heading5.getStyleClass().add("user-manual-title");
+    TextArea instructions5 = getStyledTextArea("The run steps iterations runs the game for the given number of steps. It is possible to clear out the canvas by clicking on the \"clear canvas\" button.");
+
+    container.getChildren().addAll(heading1, instructions1, heading2, instructions2, heading3, instructions3, heading4, instructions4, heading5, instructions5);
+
+    Button okayButton = new Button("Understood");
+    container.setAlignment(javafx.geometry.Pos.CENTER);
+    okayButton.setOnAction(e -> {
+      popupStage.close();
+    });
+
+    PopupScene.dimBackground(gameController.getPrimaryStage(), popupStage);
+    container.getChildren().add(okayButton);
+    popupLayout.getChildren().add(container);
+    showPopupStage(popupStage, popupLayout, 600, 600);
+    popupStage.toFront();
+  }
 
 
 }
