bc72bdb4b31101704010492a0d11569a44780bd1
Kevin Dennis Mazali <kevindm@stud.ntnu>
2024-05-21 12:34:34 +0200

    Refactor unused method in ModelValidators

--- src/main/java/edu/ntnu/idatt2003/model/utils/ModelValidators.java
+++ src/main/java/edu/ntnu/idatt2003/model/utils/ModelValidators.java
@@ -5,22 +5,6 @@ package edu.ntnu.idatt2003.model.utils;
  */
 public class ModelValidators {
 
-    /**
-     * Checks if the <code>vector</code> value is within the valid range.
-     *
-     * @param value is a Vector2D component value
-     * @param component is a string indicating whether it's x0 or x1
-     * @throws IllegalArgumentException if the value is out of range
-     */
-    public static void validateVectorValue(double value, String component) {
-        if (component.equals("x0") && (value < -1000 || value > 1000)) {
-            throw new IllegalArgumentException("x0 must be between -500 and 500");
-        } else if (component.equals("x1") && (value < -1000 || value > 1000)) {
-            throw new IllegalArgumentException("x1 must be between -500 and 500");
-        } else if (!component.equals("x0") && !component.equals("x1")) {
-            throw new IllegalArgumentException("Component must be either x0 or x1");
-        }
-    }
 
 
     public static void validateVectorValue(double value) {
